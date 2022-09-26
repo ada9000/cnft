0d099a78a7edcafed3eb331499c63f8349f02a07
jack <jack.7e@protonmail.com>
2022-09-26 20:23:11 +0100

    prettier

--- src/types.ts
+++ src/types.ts
@@ -1,50 +1,49 @@
 export type Metadata = {
-    data: Data | null
-    error: MetadataError | null
-}
+  data: Data | null;
+  error: MetadataError | null;
+};
 
 export type Data = {
-    policyId: string // TODO
-    assets: Asset[]
-    version?: NftVersions
-}
+  policyId: string; // TODO
+  assets: Asset[];
+  version?: NftVersions;
+};
 
 export type Asset = {
-    assetName: string // TODO
-    name:string
-    image: string | [string]
-    mediaType?: string
-    description?: string
-    files?: [FileMetadata]
-    other?: any
-    nftType: NftTypes
-    
-}
+  assetName: string; // TODO
+  name: string;
+  image: string | [string];
+  mediaType?: string;
+  description?: string;
+  files?: [FileMetadata];
+  other?: any;
+  nftType: NftTypes;
+};
 
 export type FileMetadata = {
-    name?: string
-    mediaType: string
-    src: string | [string]
-    other?: any
-}
+  name?: string;
+  mediaType: string;
+  src: string | [string];
+  other?: any;
+};
 
 export enum NftTypes {
-    onchain = "on-chain",
-    offchain = "off-chain",
-    ipfs = "ipfs",
-    hybrid = "hybrid",
+  onchain = 'on-chain',
+  offchain = 'off-chain',
+  ipfs = 'ipfs',
+  hybrid = 'hybrid',
 }
 
 enum NftVersions {
-    version2 = 2,
+  version2 = 2,
 }
 
 export type MetadataError = {
-    type: MetadataErrors
-    message: string
-}
+  type: MetadataErrors;
+  message: string;
+};
 
 export enum MetadataErrors {
-    json = "json",
-    cip25 = "cip25"
+  json = 'json',
+  cip25 = 'cip25',
 }
